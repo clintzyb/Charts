eef0fdb133efb2a759e14b9b1813f754d48a177f
zhu yuanbin <clintzhu@sina.com>
2017-10-20 17:31:06 +0800

    f

---
+++ Source/Charts/Charts/MyTestLine.swift
@@ -0,0 +1,20 @@
+//
+//  MyTestLine.swift
+//  Charts
+//
+//  Created by zhu yuanbin on 2017/10/20.
+//
+
+import UIKit
+
+class MyTestLine: LineChartView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
